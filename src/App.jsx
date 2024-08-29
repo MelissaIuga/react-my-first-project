937d7d24e022e918478fc4733151f5d29854119e
MelissaIuga <eaa23meiu@students.eaaa.dk>
2024-08-29 14:41:50 +0200

    My first commit of my first React App

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import User from "./components/User";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    // Fetch data from API
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers (){
+    const response = await fetch(
+      "https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"
+    );
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  return (
+    <main className='app'>
+      <h1>Users</h1>
+
+      <section className='grid'>
+        {users.map(user => (
+          <User key={user.id} user={user} />
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+export default function User({ user }) {
+    return (
+      <article className="user-card">
+        <img src={user.image || "https://placehold.co/600x400?text=Error+loading+image"} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p className="title">{user.title}</p>
+        <p>
+          <a href={`mailto:${user.mail}`}>{user.mail}</a>
+        </p>
+      </article>
+    );
+  }
